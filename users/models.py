d738d271074c7bfdedd07ef41ee8af26d79f0557
adair-jie <adair0101@163.com>
2017-05-22 21:36:58 +0800

    Complete users table design

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 #_*_ coding:utf-8 _*_
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 #继承django原有的字段，并扩展自己所需的字段
@@ -8,6 +9,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserProfile(AbstractUser):
+    '''
+    基础用户表
+    '''
     nick_name = models.CharField(max_length=50,verbose_name=u"昵称",default="")
     birthday = models.DateField(verbose_name=u"生日",null=True,blank=True)
     gender = models.CharField(max_length=5,choices=(("male",u"男"),("female",u"女")),default="female")
@@ -21,3 +25,28 @@ class UserProfile(AbstractUser):
 
     def __unicode__(self):
         return self.username
+
+class EmailVerifyRecord(models.Model):
+    '''邮箱验证码'''
+    code = models.CharField(max_length=20,verbose_name=u"验证码")
+    email = models.EmailField(max_length=50,verbose_name=u"邮箱")
+    send_type = models.CharField(choices=(("register",u"注册"),("forget",u"找回密码")),max_length=20)
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u"邮箱验证码"
+        verbose_name_plural = verbose_name
+
+class Banner(models.Model):
+        '''
+        首页轮播图
+        '''
+        title = models.CharField(max_length=100,verbose_name=u"标题")
+        image = models.ImageField(upload_to="banner/%Y/%m",verbose_name=u"轮播图",max_length=100)
+        url = models.URLField(max_length=200,verbose_name=u"访问地址")
+        index = models.IntegerField(default=100,verbose_name=u"顺序")
+        add_time = models.DateTimeField(default=datetime.now,verbose_name=u'添加时间')
+
+        class Meta:
+            verbose_name = u"轮播图"
+            verbose_name_plural = verbose_name
